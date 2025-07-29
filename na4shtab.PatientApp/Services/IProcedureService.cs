c8b6b3ee530e0e1f32fc023ee290f17aafa9c20c
Vladislav Malyshev <vladima1998@gmail.com>
2025-07-29 15:56:49 +0300

    add procedure int and logic

---
+++ na4shtab.PatientApp/Services/IProcedureService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using na4shtab.PatientApp.Models;
+
+namespace na4shtab.PatientApp.Services
+{
+    public interface IProcedureService
+    {
+        Task<List<Procedure>> GetAllAsync(string filter = null);
+
+        Task<Procedure> GetByIdAsync(int id);
+
+        Task AddAsync(Procedure procedure);
+
+        Task UpdateAsync(Procedure procedure);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ na4shtab.PatientApp/Services/ProcedureService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using na4shtab.PatientApp.Data;
+using na4shtab.PatientApp.Models;
+
+namespace na4shtab.PatientApp.Services
+{
+    public class ProcedureService : IProcedureService
+    {
+        private readonly PatientDbContext _db = new();
+
+        public async Task<List<Procedure>> GetAllAsync(string filter = null)
+        {
+            var query = _db.Procedures.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(filter))
+                query = query.Where(p => p.Name.Contains(filter));
+            return await query.ToListAsync();
+        }
+
+        public async Task<Procedure> GetByIdAsync(int id)
+            => await _db.Procedures.FindAsync(id);
+
+        public async Task AddAsync(Procedure procedure)
+        {
+            _db.Procedures.Add(procedure);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Procedure procedure)
+        {
+            _db.Procedures.Update(procedure);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var proc = await _db.Procedures.FindAsync(id);
+            if (proc != null)
+            {
+                _db.Procedures.Remove(proc);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
