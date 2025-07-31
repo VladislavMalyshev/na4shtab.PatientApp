8fff9257e895b8c1fd51caa96804bb2a455fe98d
Vladislav Malyshev <122707468+VladislavMalyshev@users.noreply.github.com>
2025-07-31 08:24:53 +0300

    Fix build and add procedure views

--- na4shtab.PatientApp/ViewModels/MainWindowViewModel.cs
+++ na4shtab.PatientApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace na4shtab.PatientApp.ViewModels;
+using ReactiveUI;
+using System.Reactive;
+
+namespace na4shtab.PatientApp.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private ViewModelBase _current = new PatientListViewModel();
+    public ViewModelBase Current
+    {
+        get => _current;
+        set => this.RaiseAndSetIfChanged(ref _current, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> ShowPatientsCmd { get; }
+    public ReactiveCommand<Unit, Unit> ShowProceduresCmd { get; }
+    public ReactiveCommand<Unit, Unit> ShowVisitsCmd { get; }
+
+    public MainWindowViewModel()
+    {
+        ShowPatientsCmd = ReactiveCommand.Create(() => { Current = new PatientListViewModel(); });
+        ShowProceduresCmd = ReactiveCommand.Create(() => { Current = new ProcedureListViewModel(); });
+        ShowVisitsCmd = ReactiveCommand.Create(() => { Current = new VisitListViewModel(); });
+    }
 }
+
---
+++ na4shtab.PatientApp/Views/ProcedureListView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace na4shtab.PatientApp.Views;
+
+public partial class ProcedureListView : UserControl
+{
+    public ProcedureListView()
+    {
+        InitializeComponent();
+    }
+}
+
